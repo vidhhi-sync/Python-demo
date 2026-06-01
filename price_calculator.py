b3ee4c85a1daec96453d9405212b8df7e223a48d
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-01 22:38:32 +0530

    Adding a price calculator

---
+++ price_calculator.py
@@ -0,0 +1,15 @@
+# Decalring variables
+"""a = 5
+   print(a)"""
+
+#Basic price calculator
+#First we will add the quantity of the product
+Quantity = 10
+print(Quantity)
+# Now, we will add the unit price of 1 product
+Unit_price= 50
+print("$",Unit_price)
+# Finally, calculating the extended price
+Extended_Price= Unit_price * Quantity
+# Showing the Extended price
+print("$",Extended_Price)
