94dfeb27b942817bd8816473dcba2fcd4f65ed41
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-06 22:08:22 +0530

    Adding Functions

---
+++ Function.py
@@ -0,0 +1,24 @@
+#Absolute Function/ value
+print(abs(5))
+print(abs(-6))
+print(abs(3.2))
+
+#Base 2 Function
+print(bin(2))
+print(bin(3))
+print(bin(7))
+
+#Base 8 Function
+print(oct(1))
+print(oct(2))
+print(oct(5))
+
+#Base 16 Function
+print(hex(10))
+print(hex(11))
+print(hex(15))
+
+#Round of
+print(round(4.9))
+print(round(3.2))
+print(round(0.1))
