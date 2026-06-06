7b688d66b957f29b62d1c4f1685ad62b02b1d0d0
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-06 18:39:01 +0530

    Practice Questions

---
+++ Practice-ques.py
@@ -0,0 +1,16 @@
+#WAP to input 2 numbers and print their sum.
+"""a=int(input("Enter value of a:"))
+b=int(input("Enter value of b:"))
+sum= a+b
+print("Sum of Given Numbers is:" , sum)
+
+#WAP tp input side of a square and print it's area
+side=int(input("Enter the value of side of square:"))
+area= side*side
+print("Area of the square is:", area)
+"""
+#WAP to input 2 floating numbers and print their average
+a=float(input("Enter value of a:"))
+b=float(input("Enter value of b:"))
+c=(a+b)/2
+print("Average of the numbers is:",c)
