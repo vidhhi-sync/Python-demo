7cf0fb2a0b45ec627981446af1e4e5b1c246092c
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-06 20:55:29 +0530

    Adding practice questions

--- Practice-ques.py
+++ Practice-ques.py
@@ -8,9 +8,15 @@ print("Sum of Given Numbers is:" , sum)
 side=int(input("Enter the value of side of square:"))
 area= side*side
 print("Area of the square is:", area)
-"""
+
 #WAP to input 2 floating numbers and print their average
 a=float(input("Enter value of a:"))
 b=float(input("Enter value of b:"))
 c=(a+b)/2
 print("Average of the numbers is:",c)
+"""
+
+#WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print False
+a=int(input("Enter value of a:"))
+b=int(input("Enter value of b:"))
+print(a>=b)
