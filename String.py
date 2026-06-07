3da863b1111f641594d9261e03d19e5c8d004813
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-07 11:45:42 +0530

    Adding string manipulation

---
+++ String.py
@@ -0,0 +1,17 @@
+#String Manipulation
+
+#cancatenating string
+num1 = "vid"
+num2 = "hi"
+sum = num1 + num2
+print("My name is ", sum)
+
+#Length of the string
+name = "Vidhi"
+print(len(name))
+name1 = "Tiya"
+print(len(name1))
+name2 = "Sanskriti"
+print(len(name2))
+name3= "vid hi" #here the space is also counted in the length of the string
+print(len(name3))
