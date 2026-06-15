4fde68487baf52ac3508ccdc9bd86a900b35af48
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-15 21:58:13 +0530

    Adding Numpy

--- Numpy.py
+++ Numpy.py
@@ -1,4 +1,5 @@
 #numpy is a library used for mathematical and statistical operations in python
+#numpy stands for numeric python
 #importing numpy
 import numpy as np
 print("successful")
@@ -8,4 +9,30 @@ list_var = [1,2,3,4,5,6]
 print(type(list_var))
 print(list_var)
 
+#one dimensional array
 #creating a list to an array
+list_to_array = np.array(list_var) #array() it is a function which returns a value to form array
+print(type(list_to_array))
+print(list_to_array) #returns the value of list but it contains no comma which signify it in form of an array
+
+#tuple to array (same)
+tuple_var = [1,2,3,4,5,6]
+print(type(tuple_var))
+print(tuple_var)
+
+tuple_to_array = np.array(list_var) #array() it is a function which returns a value to form array
+print(type(tuple_to_array))
+print(tuple_to_array)
+
+#two dimensional array
+array_2d = np.array([[1,2,3],[4,5,6]])
+print(type(array_2d))
+print(array_2d)
+
+print("\n") #\n adds a space or line between two texts
+
+#three dimensional array
+array_3d = np.array([[[1,2,3],
+                      [4,5,6],
+                      [9,8,7]]])
+print(array_3d)
