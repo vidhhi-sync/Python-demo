a634feac274e8ecc96d51c9a1526c67f2bf40aff
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-15 21:40:05 +0530

    Adding numpy

--- Numpy.py
+++ Numpy.py
@@ -1,1 +1,11 @@
-#Numpy refers to numerical python,it is an open source library (used in mathematical and statistical)
+#numpy is a library used for mathematical and statistical operations in python
+#importing numpy
+import numpy as np
+print("successful")
+
+#comparison with respect to list
+list_var = [1,2,3,4,5,6]
+print(type(list_var))
+print(list_var)
+
+#creating a list to an array
