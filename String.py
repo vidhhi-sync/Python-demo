981183a29f0f82e9a54f9715e8c52d848f9a941e
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-06-16 22:18:33 +0530

    Adding strings

--- String.py
+++ String.py
@@ -46,25 +46,31 @@ print(name4.istitle()) #checks true or false, works as is
 
 #apna college
 
+#strings
 str1 = "this is a string"
 str2 = 'this is also a string'
 str3 = """This is also considered as string"""
 print(str1)
 print(str2)
+
 #adding escape characters to modify text
 str4 = "this is a string.\nIt is used in python"
 str5 = "this is a string.\t It is used in python"
 print(str4)
 print(str5)
+
+#operations on strings
 #performing concatenation on string
 print(str1 + " " + str2) #this empty quotation marks adds a space.
 #printing length of a string
 print(len(str1)) #it counts the spaces in between also.
+
 #Indexing
 str6 = "Vidhi"
 ch = str6[0] #index 0 is the position of first character
 print(ch) #prints the character whose index we are calling, if there is a space we can print it too
 print(str6[3])
+
 #slicing
 print(str6[0:3]) #it prints characters on index 0,1,2 , it won't print 3 index character
 print(str6[0:len(str6)]) #when we want to print the last character in string
