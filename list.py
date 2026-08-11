bafbea86bcd6cdde09850feb4ad25eac1a874f8d
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 10:20:30 +0530

    Adding list functions

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+#lists code 1
 a = [10,20,30,40]
 print(a)
 a.insert(1,15)
@@ -12,3 +13,32 @@ print(a)
 a.clear()
 print(a)
 
+#dictionary code 2
+people = {
+    'vb' : 'vidhi bansal',
+    'lb' : 'lalita bansal'    
+}
+print(people)
+print(len(people))
+
+
+#tuple code 3
+b = (10,20,30)
+print(b)
+print(len(b))
+print(b[1])
+
+
+#list
+my_list = [1,2,3]
+my_list.append(int(input("Enter element: ")))
+print(my_list)
+my_list.insert(1, int(input("Enter element: ")))
+print(my_list)
+my_list.remove(int(input("Enter element: ")))
+print(my_list)
+my_list.clear()
+print(my_list)
+my_list.pop
+print(my_list)
+print(len(my_list))
